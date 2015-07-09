6d07c4be166f08c264d6c00b073070ef09ab5913
Tiagojdferreira <tiagojdferreira@gmail.com>
2015-07-09 22:23:03 +0200

    testing new architecture

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
 
-gulp.task('default',function(){
+gulp.task('default',['js']);
 
+gulp.task('js',function(){
+  return gulp.src('./src/**/*.js')
+    .pipe(gulp.dest('./www/js/'));
+});
+gulp.task('vendor',function(){
+  return gulp.src('./node_modules/**/*.min.js')
+    .pipe(gulp.dest('./www/vendor/'));
 });
